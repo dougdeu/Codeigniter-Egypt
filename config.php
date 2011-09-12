cc5146ffb2c00dc27e34dc85b0609f3c100b400f
dougdeu <ddeurioste@engineyard.com>
2011-09-12 10:02:09 -0700

    Edited config.php via GitHub

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $config_site_name		= "Site Name";
 
-$db_hostname			= "localhost";
-$db_database			= "egypt";
-$db_username			= "root";
-$db_password			= "1234";
+$db_hostname			= "10.194.111.8";
+$db_database			= "db_9a2c5781";
+$db_username			= "user_9a2c5781";
+$db_password			= "vns9$1iXNwShOU";
 $db_dbdriver			= "mysql";
